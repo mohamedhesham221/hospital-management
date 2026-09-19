6ea0091448776c830cd5f17b0ec8b63db18a15a0
Muhammad Hisham <muhammedheshamm2@gmail.com>
2026-09-20 00:05:00 +0300

    add new password page

--- app/(auth)/new-password/page.tsx
+++ app/(auth)/new-password/page.tsx
@@ -1,9 +1,39 @@
+import { Button } from "@/components/ui/button"
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
 export default function NewPasswordPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-bold">New Password Page</h1>
-      <p className="mt-4 text-lg">Please enter your new password.</p>
-      {/* Add your new password form or logic here */}
-    </div>
-  );
+    <>
+      <div className="flex flex-col items-center justify-center py-2">
+        <h1 className="font-inter text-2xl font-bold">Create New Password</h1>
+        <p className="text-md mt-2 mb-4 text-gray-500">Please enter a new password.</p>
+        {/* Add your new password form or logic here */}
+        <form className="w-full max-w-xs">
+          <FieldSet className="w-full max-w-xs">
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input id="password" type="password" placeholder="••••••••" />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="confirmPassword">
+                  Confirm Password
+                </FieldLabel>
+                <Input
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="••••••••"
+                />
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+
+          <Button className="mt-4 w-full cursor-pointer bg-blue-500 hover:bg-blue-600">
+            Reset Password
+          </Button>
+        </form>
+      </div>
+    </>
+  )
 }
